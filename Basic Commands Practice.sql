8cdd928c2c3435df2cc57f1c190a7ea6c0800e05
Shawn <38596180+gshawnr@users.noreply.github.com>
2019-08-13 17:31:12 -0700

    Add files via upload

---
+++ Basic Commands Practice.sql
@@ -0,0 +1,38 @@
+-- using the sql_store db get all customers born before 1990 and organize by state in reverse order (returns 7 records)
+-- return products with quantity in stock equal to 49, 38, 72 (ANS IS PORK AND LETTUCE)
+-- return customers born between Jan 1, 1990 and Jan 1, 2000 (should return 3 items)
+-- get customers whose addresses contain TRAIL or AVENUE using both LIKE and REGEXP (returns 3 item)
+-- get the customers whose first names match:
+		-- first names are elka or ambur (2 matches) 
+		-- last names end with EY or ON (4 matches)    
+		-- last names start with MY or contains SE (3 matches)
+		-- last names contain B followed by R or U (2 matches)
+-- write a query to get all orders that are not shipped (5 matches)
+-- write a query to get the top 3 (by points) customers (returns customers 5, 6, 3)
+-- write a query to join the order_items and products table.  Get all from order_items and product name from products
+-- write a query to join the payments table with the payment methods table and the clients table (invoiceing DB)
+-- Write a query to outer join the order_items and products tables.  Show product_id, name, quantity.  Show all products.
+-- Write a join statement with a using statement on sql_invoiceing database (payments and clients).  Returns 8
+-- Do a cross join between shippers and products.  Both explicit and implicit syntax
+-- Unions (combine rows instead of columns) 
+		-- Write a query that adds a column 'type' and assigns bronze, silver, gold based on points and displays all results
+-- Write a statement to input 3 rows into the products table
+-- Write a statement to copy a table (note does not include same table constraints such as pk or auto increment)
+-- Write a statement that combines invoices and clients into a new table where invoices have recieved a least one payment
+		-- returns 7 records 
+-- Write a statement to update the phone number in the customers table for Clemmie Betchley.  Also update her last name
+		-- to match her firstname.  Once done, reverse the changes.
+-- Write a statement to give any customers born before 1990 50 extra points.
+-- Write a statement to update the comments column of the orders table to say "Gold Customer" where the customer
+		-- has more than 3000 points
+-- Write a delete statement to delete an invoice for a client with a given firstname (be sure to add it back after)
+
+
+
+
+
+    
+
+
+
+    
